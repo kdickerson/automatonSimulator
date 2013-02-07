4d46323df68ec5fbf76b705de2e59a5932f078a9
Kyle Dickerson <kyle.dickerson@gmail.com>
2013-02-06 20:52:13 -0800

    Implement methods on nfa_delegate. Still need to implement UpdateStatusUI.

--- js/nfa_delegate.js
+++ js/nfa_delegate.js
@@ -1,11 +1,20 @@
 var nfa_delegate = (function() {
   var self = null;
-  var dfa = null;
+  var nfa = null;
+  var container = null;
+  var emptyLabel = '[empty]';
+  
+  var statusConnectors = [];
 
   var updateStatusUI = function(status, curState) {
-    var doneSpan = $('<span id="consumedInput"></span>').html(status.input.substring(0, status.inputIndex));
-    var curSpan = $('<span id="currentInput"></span>').html(status.input.substr(status.inputIndex, 1));
-    var futureSpan = $('<span id="futureInput"></span>').html(status.input.substring(status.inputIndex+1));
+    // TODO: Test for existing box
+    //  If found, add to it, and reposition
+    //  Position based on actual height, not just magic 25px value
+    //    Backport that change to DFA code
+    
+    var doneSpan = $('<span class="consumedInput"></span>').html(status.input.substring(0, status.inputIndex));
+    var curSpan = $('<span class="currentInput"></span>').html(status.input.substr(status.inputIndex, 1));
+    var futureSpan = $('<span class="futureInput"></span>').html(status.input.substring(status.inputIndex+1));
     
     if (curState.length > 0) {
       $('#dfaStatus').css('left', curState.position().left + 4 + 'px')
@@ -24,46 +33,96 @@ var nfa_delegate = (function() {
   
   var updateUIForDebug = function() {
     var status = dfa.status();
+    
     $('.current').removeClass('current');
-    var curState = $('#' + status.state).addClass('current');
-    updateStatusUI(status, curState);
+    $.each(statusConnectors, function(index, connection) {
+      connection.setPaintStyle(jsPlumb.Defaults.PaintStyle);
+    });
     
-    
+    if (status.status === 'Active') {
+      $.each(status.stateIndexPairs, function(index, pair) {
+        var curState = $('#' + pair.state).addClass('current');
+        jsPlumb.select({source:pair.state}).each(function(connection) {
+          if (connection.getLabel() === status.input.substr(pair.index, 1)) {
+            statusConnectors.push(connection);
+            connection.setPaintStyle({strokeStyle:'#0a0'});
+          }
+        });
+        updateStatusUI(status, curState);
+      });
+    }
+    return self;
   };
 
   return {
     init: function() {
       self = this;
-      dfa = new DFA();
+      nfa = new NFA();
+      return self;
+    },
+    
+    setContainer: function(newContainer) {
+      container = newContainer;
       return self;
     },
     
     fsm: function() {
-      return dfa;
+      return nfa;
     },
     
     connectionAdded: function(info) {
+      // TODO: Better UI
       var inputChar = prompt('Read what input character on transition?', 'A');
       inputChar = (inputChar && inputChar.length > 0) ? inputChar[0] : inputChar;
-      if (!inputChar || dfa.hasTransition(info.sourceId, inputChar)) {
+      if (inputChar === null || nfa.hasTransition(info.sourceId, inputChar, info.targetId)) {
         jsPlumb.detach(info.connection);
-        if (inputChar && inputChar.length === 0) {
-          alert("Deterministic Finite Automaton cannot have empty string transition.");
-        } else if (inputChar) {
+        if (inputChar) {
           alert(info.sourceId + " already has a transition for " + inputChar);
         }
         return;
       } 
-      info.connection.setPaintStyle({strokeStyle:"#0a0"});
-      info.connection.getOverlay("label").setLabel(inputChar);
-      dfa.addTransition(info.sourceId, inputChar, info.targetId);
+      info.connection.setLabel(inputChar || emptyLabel);
+      nfa.addTransition(info.sourceId, inputChar, info.targetId);
     },
     
     updateUI: updateUIForDebug,
     
     reset: function() {
-      dfa = new DFA();
+      nfa = new NFA();
       return self;
+    },
+    
+    debugStart: function() {
+      return self;
+    },
+    
+    debugStop: function() {
+      $('.fsmStatus').remove();
+      return self;
+    },
+    
+    serialize: function() {
+      // Convert dfa into common serialized format
+      var model = {};
+      model.type = 'NFA';
+      model.nfa = nfa.serialize();
+      model.states = {};
+      model.transitions = [];
+      $.each(model.dfa.transitions, function(stateA, transition) {
+        model.states[stateA] = {};
+        $.each(transition, function(character, stateB) {
+          model.states[stateB] = {};
+          model.transitions.push({stateA:stateA, label:(character || emptyLabel), stateB:stateB});
+        });
+      });
+      $.each(model.nfa.acceptStates, function(index, state) {
+        model.states[state].isAccept = true;
+      });
+      return model;
+    },
+    
+    deserialize: function(model) {
+      nfa.deserialize(model.nfa);
     }
   };
 }()).init();
