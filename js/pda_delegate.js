39bd443ce515ee9490436cc28ff0f0137cb887f8
Kyle Dickerson <kyle.dickerson@gmail.com>
2013-02-18 21:45:56 -0800

    Implement the PDA delegate. Need to include stacks in debug UI updates.

--- js/pda_delegate.js
+++ js/pda_delegate.js
@@ -1,69 +1,123 @@
 var pda_delegate = (function() {
   var self = null;
-  var dfa = null;
-
-  var updateStatusUI = function(status, curState) {
-    var doneSpan = $('<span id="consumedInput"></span>').html(status.input.substring(0, status.inputIndex));
-    var curSpan = $('<span id="currentInput"></span>').html(status.input.substr(status.inputIndex, 1));
-    var futureSpan = $('<span id="futureInput"></span>').html(status.input.substring(status.inputIndex+1));
-    
-    if (curState.length > 0) {
-      $('#dfaStatus').css('left', curState.position().left + 4 + 'px')
-        .css('top', curState.position().top - 25 + 'px')
-        .html('').append(doneSpan).append(curSpan).append(futureSpan);
-        
-      if ($('#dfaStatus').position().top < 0) { // Flip to bottom
-        $('#dfaStatus').css('top', $('#dfaStatus').position().top + curState.outerHeight() + 29 + 'px');
-      }
-      var overscan = $('#dfaStatus').position().left + $('#dfaStatus').outerWidth() + 4 - $('#machineGraph').innerWidth();
-      if (overscan > 0) { // Push inward
-        $('#dfaStatus').css('left', $('#dfaStatus').position().left - overscan + 'px');
-      }
-    };
+  var pda = null;
+  var container = null;
+  var emptyLabel = '[empty]';
+  
+  var statusConnectors = [];
+  
+  var makeConnectionLabel = function(inputChar, popChar, pushChar) {
+    return (inputChar || emptyLabel) + ',' + (popChar || emptyLabel) + ',' + (pushChar || emptyLabel);
   };
   
   var updateUIForDebug = function() {
-    var status = dfa.status();
+    var status = pda.status();
+    
     $('.current').removeClass('current');
-    var curState = $('#' + status.state).addClass('current');
-    updateStatusUI(status, curState);
+    $.each(statusConnectors, function(index, connection) {
+      connection.setPaintStyle(jsPlumb.Defaults.PaintStyle);
+    });
     
-    
+    if (status.status === 'Active') {
+      $.each(status.stateStackPairs, function(index, ssp) {
+        var curState = $('#' + ssp.state).addClass('current');
+        var sspLabelParts = makeConnectionLabel(status.nextChar, ssp.stack[0], '').split(',');
+        jsPlumb.select({source:ssp.state}).each(function(connection) {
+          var connLabelParts = connection.getLabel().split(','); // Don't care about the pushChar here
+          if (connLabelParts[0] === sspLabelParts[0] && connLabelParts[1] === sspLabelParts[1]) {
+            statusConnectors.push(connection);
+            connection.setPaintStyle({strokeStyle:'#0a0'});
+          }
+        });
+      });
+    }
+    return self;
   };
 
   return {
     init: function() {
       self = this;
-      dfa = new DFA();
+      pda = new PDA();
+      return self;
+    },
+    
+    setContainer: function(newContainer) {
+      container = newContainer;
       return self;
     },
     
     fsm: function() {
-      return dfa;
+      return pda;
     },
     
     connectionAdded: function(info) {
-      var inputChar = prompt('Read what input character on transition?', 'A');
-      inputChar = (inputChar && inputChar.length > 0) ? inputChar[0] : inputChar;
-      if (!inputChar || dfa.hasTransition(info.sourceId, inputChar)) {
+      // TODO: Better UI
+      var chars = prompt('Transition characters (format: input,pop,push):', 'A,B,C');
+      var inputChar=null, popChar=null, pushChar = null;
+      if (chars !== null) {
+        chars = chars.split(',');
+        if (chars.length === 3) {
+          inputChar = chars[0];
+          popChar = chars[1];
+          pushChar = chars[2];
+        }
+      }
+      
+      // either they'll all be null or they'll all be non-null, so only need to test one
+      if (inputChar === null || pda.hasTransition(info.sourceId, inputChar, popChar, pushChar, info.targetId)) {
         jsPlumb.detach(info.connection);
-        if (inputChar && inputChar.length === 0) {
-          alert("Deterministic Finite Automaton cannot have empty string transition.");
-        } else if (inputChar) {
-          alert(info.sourceId + " already has a transition for " + inputChar);
+        if (inputChar !== null) {
+          alert(info.sourceId + " already has a transition to " + info.targetId + " on " + 
+            makeConnectionLabel(inputChar, popChar, pushChar));
         }
         return;
       } 
-      info.connection.setPaintStyle({strokeStyle:"#0a0"});
-      info.connection.getOverlay("label").setLabel(inputChar);
-      dfa.addTransition(info.sourceId, inputChar, info.targetId);
+      info.connection.setLabel(makeConnectionLabel(inputChar, popChar, pushChar));
+      pda.addTransition(info.sourceId, inputChar, popChar, pushChar, info.targetId);
     },
     
     updateUI: updateUIForDebug,
     
     reset: function() {
-      dfa = new DFA();
+      pda = new PDA();
       return self;
+    },
+    
+    debugStart: function() {
+      return self;
+    },
+    
+    debugStop: function() {
+      $('.current').removeClass('current');
+      return self;
+    },
+    
+    serialize: function() {
+      // Convert dfa into common serialized format
+      var model = {};
+      model.type = 'PDA';
+      model.pda = pda.serialize();
+      model.states = {};
+      model.transitions = [];
+      $.each(model.pda.transitions, function(stateA, inputCharBase) {
+        model.states[stateA] = {};
+        $.each(inputCharBase, function(inputChar, popCharBase) {
+          $.each(popCharBase, function(popChar, stateStackPairs) {
+            $.each(stateStackPairs, function(idx, ssp) {
+              model.states[ssp.state] = {};
+              model.transitions.push({stateA:stateA, label:makeConnectionLabel(inputChar, popChar, pushChar), stateB:ssp.state});
+            });
+          });
+        });
+      });
+      $.each(model.pda.acceptStates, function(index, state) {
+        model.states[state].isAccept = true;
+      });
+      return model;
+    },
+    
+    deserialize: function(model) {
+      pda.deserialize(model.pda);
     }
   };
 }()).init();
